582cedd396ff2d22774d34942534dea64d93558b
Faiyaz Rahman <faiyazrahman03@gmail.com>
2021-10-08 12:30:46 +0600

    added comments for better readable code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express');
+//dependencies
 const mongoose = require('mongoose');
 
+//* Database Connection
+//? by changing the mongodb connection string you can connect to your own database
 mongoose
    .connect(
       'mongodb+srv://faiyaz:tncZi0cn9skkOnbJ@cluster0.peotm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -8,6 +10,7 @@ mongoose
    .then(() => console.log(`app is successfully connected to database`))
    .catch((error) => console.log(error));
 
+//* Coordinates model and schema
 const cordsSchema = new mongoose.Schema({
    name: { type: String, unique: true },
    location: {
@@ -16,20 +19,24 @@ const cordsSchema = new mongoose.Schema({
    },
 });
 cordsSchema.index({ location: '2dsphere' });
-
 const Coordinates = mongoose.model('coordinate', cordsSchema);
 
+//* Function to convert kilometer to earth radian
 const kmsToRadian = function (kms) {
    var earthRadiusInKms = 6371;
    return kms / earthRadiusInKms;
 };
 
+//* By changing this variable you can change the value of radius as your need. Make sure to pass the radius value on KILOMETERS
+const radiusInKilometers = 0.1;
+
+//* Main function to fetch the coordinates from database within a cetrain radius. In that case it is 100 meter or 0.1 kilometers
 const fetchCords = async function (data) {
    try {
       const cords = await Coordinates.find({
          location: {
             $geoWithin: {
-               $centerSphere: [[data.coordinates[1], data.coordinates[0]], kmsToRadian(0.1)],
+               $centerSphere: [[data.coordinates[1], data.coordinates[0]], kmsToRadian(radiusInKilometers)],
             },
          },
       }).select({ __v: 0 });
